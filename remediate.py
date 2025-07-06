575395082fd584ae334d2e2d61f93d94d20a5f24
prakhar-devops619 <prakharrawat1906@gmail.com>
2025-07-06 17:44:53 +0530

    Update remediate.py

--- remediate.py
+++ remediate.py
@@ -1,1 +1,8 @@
-# Restart service/container
+import subprocess
+
+def restart_service(service_name):
+    try:
+        subprocess.run(["systemctl", "restart", service_name], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
